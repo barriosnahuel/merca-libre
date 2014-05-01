cb33771c51ab19f0a364d6573c84dbda6934bd82
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-05-01 17:39:15 -0300

    Actualizo el DER, y genero los scripts para que ya haya de ejemplo.

---
+++ bd/generado/usuarios y roles-insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO [GD1C2014].[dbo].[ROL]
+           ([NOMBRE])
+     VALUES
+           ('Administrativo');
+           
+INSERT INTO [GD1C2014].[dbo].[ROL]
+           ([NOMBRE])
+     VALUES
+           ('Cliente');
+           
+INSERT INTO [GD1C2014].[dbo].[ROL]
+           ([NOMBRE])
+     VALUES
+           ('Empresa');
+GO
+
+
