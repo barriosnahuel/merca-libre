b5fc1bc70a90a33772124df83e7a1678d97ab131
kevin furman <kaf.com@hotmail.com>
2014-05-03 18:08:17 -0300

    avance funciones

---
+++ app/FrbaCommerce/Modelos/Funcion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FrbaCommerce.Modelos
+{
+    public class Funcion
+    {
+        Int32 _id;
+        String _nombre;
+
+        public Int32 id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public String nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+    }
+}
---
+++ app/FrbaCommerce/Servicios/Funciones.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FrbaCommerce.Modelos;
+using Utiles;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FrbaCommerce.Servicios
+{
+    class Funciones
+    {
+
+        private static Funcion getFuncion(SqlDataReader lector)
+        {
+            Funcion unaFuncion = new Funcion();
+            unaFuncion.id = lector.GetInt32(lector.GetOrdinal("ID"));
+            unaFuncion.nombre = lector.GetString(lector.GetOrdinal("NOMBRE"));
+
+            return unaFuncion;
+        }
+
+        public static List<Funcion> ObtenerTodas()
+        {
+            List<Funcion> listaFunciones = new List<Funcion>();
+
+            SqlDataReader lector = BasesDeDatos.ObtenerDataReader("SELECT * FROM FUNCIONALIDAD");
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    Funcion unRol = getFuncion(lector);
+                    listaFunciones.Add(unRol);
+                }
+            }
+
+            lector.Close();
+
+            return listaFunciones;
+        }
+
+        public static List<Funcion> ObtenerPorRol(Int32 idRol)
+        {
+            List<Funcion> listaFunciones = new List<Funcion>();
+
+            SqlDataReader lector = BasesDeDatos.ObtenerDataReader("SELECT * FROM FUNCIONALIDAD F JOIN FUNCIONALIDAD_X_ROL FR ON F.ID = FR.FUNCIONALIDAD_ID WHERE FR.ROL_ID = " + idRol);
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    Funcion unRol = getFuncion(lector);
+                    listaFunciones.Add(unRol);
+                }
+            }
+
+            lector.Close();
+
+            return listaFunciones;
+        }
+
+        public static List<Funcion> ObtenerNotRol(Int32 idRol)
+        {
+            List<Funcion> listaFunciones = new List<Funcion>();
+
+            SqlDataReader lector = BasesDeDatos.ObtenerDataReader("SELECT * FROM FUNCIONALIDAD F JOIN FUNCIONALIDAD_X_ROL FR ON F.ID = FR.FUNCIONALIDAD_ID WHERE FR.ROL_ID <> " + idRol);
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    Funcion unRol = getFuncion(lector);
+                    listaFunciones.Add(unRol);
+                }
+            }
+
+            lector.Close();
+
+            return listaFunciones;
+        }
+
+    }
+}
