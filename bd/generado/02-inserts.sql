c5251af8b2e7b9cc794190afa7a10b88d305db88
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-05-02 21:59:21 -0300

    "Termino" el DER y los scripts de creación, más los inserts de los datos maestros que no hay que cargar desde la tabla inicial. Todavía le queda al EAP settear el esquema.

--- bd/generado/02-inserts.sql
+++ bd/generado/02-inserts.sql
@@ -1,17 +1,63 @@
+--	Inserts para tabla maestra ROL
 INSERT INTO [GD1C2014].[dbo].[ROL]
            ([NOMBRE])
      VALUES
-           ('Administrativo');
+           ('Administrativo')
            
 INSERT INTO [GD1C2014].[dbo].[ROL]
            ([NOMBRE])
      VALUES
-           ('Cliente');
+           ('Cliente')
            
 INSERT INTO [GD1C2014].[dbo].[ROL]
            ([NOMBRE])
      VALUES
-           ('Empresa');
+           ('Empresa')
 GO
 
 
+--	Inserts para tabla maestra ESTADO
+INSERT INTO [GD1C2014].[dbo].[ESTADO]
+           ([DESCRIPCION])
+     VALUES
+           ('Borrador')
+INSERT INTO [GD1C2014].[dbo].[ESTADO]
+           ([DESCRIPCION])
+     VALUES
+           ('Publicada')
+INSERT INTO [GD1C2014].[dbo].[ESTADO]
+           ([DESCRIPCION])
+     VALUES
+           ('Pausada')
+INSERT INTO [GD1C2014].[dbo].[ESTADO]
+           ([DESCRIPCION])
+     VALUES
+           ('Finalizada')
+GO
+
+--	Inserts para tabla maestra TIPO_PUBLICACION
+INSERT INTO [GD1C2014].[dbo].[TIPO_PUBLICACION]
+           ([DESCRIPCION])
+     VALUES
+           ('Compra inmediata')
+INSERT INTO [GD1C2014].[dbo].[TIPO_PUBLICACION]
+           ([DESCRIPCION])
+     VALUES
+           ('Subasta')
+GO
+
+
+--	Inserts para tabla maestra RUBRO
+INSERT INTO [GD1C2014].[dbo].[RUBRO]
+           ([DESCRIPCION])
+     VALUES
+           ('Entretenimiento')
+INSERT INTO [GD1C2014].[dbo].[RUBRO]
+           ([DESCRIPCION])
+     VALUES
+           ('Hogar')
+INSERT INTO [GD1C2014].[dbo].[RUBRO]
+           ([DESCRIPCION])
+     VALUES
+           ('Cuidado personal')
+GO
