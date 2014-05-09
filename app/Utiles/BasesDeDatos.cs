529c5d91fd071280671217e24ca11e4f6332d86d
kevin furman <kaf.com@hotmail.com>
2014-05-09 17:39:21 -0300

    No se que pasa, no puedo hacer andar con archivo de configuracion

--- app/Utiles/BasesDeDatos.cs
+++ app/Utiles/BasesDeDatos.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace Utiles
 {
@@ -12,11 +13,13 @@ namespace Utiles
     {
 
         static SqlConnection Conexion = new SqlConnection();
+        static String connString = @"Server=localhost\SQLSERVER2008;Database=GD1C2014;User Id=gd;Password=gd2014;";
+
         public static SqlConnection ObtenerConexion()
         {
             if (Conexion.State == ConnectionState.Closed)
             {
-                Conexion.ConnectionString = LeerConfiguracion("Provider", @"Server=localhost\SQLSERVER2008;Database=GD1C2014;User Id=gd;Password=gd2014;");
+                Conexion.ConnectionString = connString;
                 Conexion.Open();
             }
 
@@ -32,7 +35,7 @@ namespace Utiles
             else
             {
                 SqlConnection Conexion = new SqlConnection();
-                Conexion.ConnectionString = LeerConfiguracion("Provider", @"Server=localhost\SQLSERVER2008;Database=GD1C2014;User Id=gd;Password=gd2014;");
+                Conexion.ConnectionString = connString;
                 Conexion.Open();
                 return Conexion;
             }
@@ -173,144 +176,13 @@ namespace Utiles
             {
                 return false;
             }
-
-
         }
-
-
 
         public static bool EscribirEnBase(string SQL)
         {
             List<SqlParameter> ListaParametros = new List<SqlParameter>();
             return EscribirEnBase(SQL, TiposEscritura.Text, ListaParametros);
         }
-
-        public static string LeerConfiguracion(string nombre)
-        {
-            string Valor = "";
-            bool ValorEncontrado = false;
-
-            if (!File.Exists(Application.StartupPath + @"\Configuracion.txt"))
-            {
-                StreamWriter escritor = new StreamWriter(Application.StartupPath + @"\Configuracion.txt");
-                escritor.Close();
-
-            }
-
-            StreamReader lector = new StreamReader(Application.StartupPath + @"\Configuracion.txt");
-            string linea = lector.ReadLine();
-            while (linea != null)
-            {
-                if (linea.Substring(0, linea.IndexOf("_")) == nombre)
-                {
-                    Valor = linea.Substring(linea.IndexOf("_") + 1);
-                    ValorEncontrado = true;
-                }
-                linea = lector.ReadLine();
-            }
-            lector.Close();
-
-
-
-            if (ValorEncontrado == true)
-            {
-                return Valor;
-            }
-            else
-            {
-                return null;
-            }
-
-        }
-
-        public static string LeerConfiguracion(string nombre, string valordefault)
-        {
-            string Valor = "";
-            bool ValorEncontrado = false;
-
-            if (!File.Exists(Application.StartupPath + @"Configuracion.txt"))
-            {
-                StreamWriter escritor = new StreamWriter(Application.StartupPath + @"Configuracion.txt");
-                escritor.Close();
-
-            }
-
-            StreamReader lector = new StreamReader(Application.StartupPath + @"Configuracion.txt");
-            string linea = lector.ReadLine();
-            while (linea != null)
-            {
-                if (linea.Substring(0, linea.IndexOf("_")) == nombre)
-                {
-                    Valor = linea.Substring(linea.IndexOf("_") + 1);
-                    ValorEncontrado = true;
-                }
-                linea = lector.ReadLine();
-            }
-            lector.Close();
-
-
-
-            if (ValorEncontrado == true)
-            {
-                return Valor;
-            }
-            else
-            {
-                GrabarConfiguracion(nombre, valordefault);
-                return valordefault;
-            }
-
-        }
-
-
-        public static void GrabarConfiguracion(string nombre, string valor)
-        {
-            if (!File.Exists(Application.StartupPath + @"\Configuracion.txt"))
-            {
-                StreamWriter escritor = new StreamWriter(Application.StartupPath + @"\Configuracion.txt");
-                escritor.Close();
-            }
-
-            StreamReader leer = new StreamReader(Application.StartupPath + @"\Configuracion.txt");
-            List<string> Configs = new List<string>();
-            string linea = leer.ReadLine();
-            while (linea != null)
-            {
-                Configs.Add(linea);
-                linea = leer.ReadLine();
-            }
-            leer.Close();
-
-            if (LeerConfiguracion(nombre) == null)
-            {
-                StreamWriter escritor = new StreamWriter(Application.StartupPath + @"\Configuracion.txt");
-                escritor.WriteLine(nombre + "_" + valor);
-                escritor.Close();
-
-            }
-            else
-            {
-                for (int i = 0; i < Configs.Count; i++)
-                {
-                    if (Configs[i].Substring(0, Configs[i].IndexOf("_")) == nombre)
-                    {
-                        Configs[i] = nombre + "_" + valor;
-                        i = Configs.Count + 5;
-                    }
-                }
-
-                StreamWriter escritor = new StreamWriter(Application.StartupPath + @"\Configuracion.txt");
-                for (int i = 0; i < Configs.Count; i++)
-                {
-                    escritor.WriteLine(Configs[i]);
-
-                }
-
-                escritor.Close();
-
-            }
-        }
-
 
     }
 
