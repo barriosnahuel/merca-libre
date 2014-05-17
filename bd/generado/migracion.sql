d2265e1038966af80138642a26ccb74ed2529a13
kevin furman <kaf.com@hotmail.com>
2014-05-16 21:31:07 -0300

    Avance de la migracion

--- bd/generado/migracion.sql
+++ bd/generado/migracion.sql
@@ -68,6 +68,8 @@ FETCH NEXT FROM maestra_cursor
 INTO @Publ_Cli_Dni, @Publ_Cli_Apellido, @Publ_Cli_Nombre, @Publ_Cli_Fecha_Nac, @Publ_Cli_Mail, @Publ_Cli_Dom_Calle, @Publ_Cli_Nro_Calle, @Publ_Cli_Piso, @Publ_Cli_Depto, @Publ_Cli_Cod_Postal, @Publ_Empresa_Razon_Social, @Publ_Empresa_Cuit, @Publ_Empresa_Fecha_Creacion, @Publ_Empresa_Mail, @Publ_Empresa_Dom_Calle, @Publ_Empresa_Nro_Calle, @Publ_Empresa_Piso, @Publ_Empresa_Depto, @Publ_Empresa_Cod_Postal, @Publicacion_Cod, @Publicacion_Descripcion, @Publicacion_Stock, @Publicacion_Fecha, @Publicacion_Fecha_Venc, @Publicacion_Precio, @Publicacion_Tipo, @Publicacion_Visibilidad_Cod, @Publicacion_Visibilidad_Desc, @Publicacion_Visibilidad_Precio, @Publicacion_Visibilidad_Porcentaje, @Publicacion_Estado, @Publicacion_Rubro_Descripcion, @Cli_Dni, @Cli_Apeliido, @Cli_Nombre, @Cli_Fecha_Nac, @Cli_Mail, @Cli_Dom_Calle, @Cli_Nro_Calle, @Cli_Piso, @Cli_Depto, @Cli_Cod_Postal, @Compra_Fecha, @Compra_Cantidad, @Oferta_Fecha, @Oferta_Monto, @Calificacion_Codigo, @Calificacion_Cant_Estrellas, @Calificacion_Descripcion, @Item_Factura_Monto, @Item_Factura_Cantidad, @Factura_Nro, @Factura_Fecha, @Factura_Total, @Forma_Pago_Desc
 
 DECLARE @Current_Publicacion_Cod NUMERIC(18, 0);
+DECLARE @username nvarchar(100);
+DECLARE @dir nvarchar(max);
 DECLARE @Publ_Owner bigint; -- ID del usuario duenio de la publicacion
 SET @Current_Publicacion_Cod = 0;
 WHILE @@FETCH_STATUS = 0
@@ -95,8 +97,6 @@ WHILE @@FETCH_STATUS = 0
 --		FACTURA_ITEM
 --		FORMA_PAGO
 
-		
-
 
         IF (@Current_Publicacion_Cod <> @Publicacion_Cod)
         BEGIN
@@ -105,13 +105,10 @@ WHILE @@FETCH_STATUS = 0
             
             IF (@Publ_Cli_Dni IS NOT NULL)
             BEGIN            
-				--Guardar el cliente si no existe
+				--Guardar el cliente si no existe, sino seteo el @Publ_Owner con el id existente
 				IF NOT EXISTS(SELECT 1 FROM GOODTIMES.CLIENTE WHERE DNI = @Publ_Cli_Dni)
 				BEGIN                        
-					DECLARE @username nvarchar(100)
 					SET @username = GOODTIMES. GET_UNIQUE_USERNAME(@Publ_Cli_Nombre)
-					DECLARE @dir nvarchar(max)
-					PRINT @username
 					SET @dir = @Publ_Cli_Dom_Calle + ' ' + CONVERT(varchar, @Publ_Cli_Nro_Calle) + ' ' + CONVERT(varchar, @Publ_Cli_Piso) + ' ' + CONVERT(varchar, @Publ_Cli_Depto)
 					
 					EXEC [GOODTIMES].[CrearCliente] @Publ_Cli_Nombre, @Publ_Cli_Apellido, @Publ_Cli_Dni, 'DNI', @Publ_Cli_Fecha_Nac, @username, "123456", 0, 1, 0, @Publ_Cli_Mail, '', @dir, @Publ_Cli_Cod_Postal, 'Buenos Aires'
@@ -126,13 +123,25 @@ WHILE @@FETCH_STATUS = 0
 
             IF (@Publ_Empresa_Razon_Social IS NOT NULL)
             BEGIN
-				PRINT 'Guardar publicacion y asociarla a la empresa'
+				--Guardar empresa si no existe, sino seteo el @Publ_Owner con el id existente
+				IF NOT EXISTS(SELECT 1 FROM GOODTIMES.EMPRESA WHERE CUIT = @Publ_Empresa_Cuit)
+				BEGIN                        
+					SET @username = GOODTIMES. GET_UNIQUE_USERNAME(@Publ_Cli_Nombre)
+					SET @dir = @Publ_Empresa_Dom_Calle + ' ' + CONVERT(varchar, @Publ_Empresa_Nro_Calle) + ' ' + CONVERT(varchar, @Publ_Empresa_Piso) + ' ' + CONVERT(varchar, @Publ_Empresa_Depto)
+					
+					EXEC [GOODTIMES].CrearEmpresa @Publ_Empresa_Razon_Social, @Publ_Empresa_Cuit, '', @Publ_Empresa_Fecha_Creacion, @username, '123456', 0, 1, 0, @Publ_Empresa_Mail, '', @dir, @Publ_Empresa_Cod_Postal, 'Buenos Aires'					
+					SET @Publ_Owner = @@IDENTITY;					
+				END
+				ELSE
+				BEGIN
+					SET @Publ_Owner = (SELECT ID FROM GOODTIMES.EMPRESA WHERE CUIT = @Publ_Empresa_Cuit);
+				END	
             END
 
--- Guardar y Asociar PUBLICACION al USUARIO @Publ_Owner
+			-- Guardar y Asociar PUBLICACION al USUARIO @Publ_Owner
 
                 
-         END
+        END
 
 -- Asociar VISIBILIDAD_PUBLICACION
 -- Asociar TIPO_PUBLICACION
