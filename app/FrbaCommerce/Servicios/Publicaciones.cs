894c30ed1800a93985f2509f54292a105ba72531
clausi laureano <laureano.clausi@gmail.com>
2014-05-21 18:24:25 -0300

    Clases para servicios de publicaciones.

---
+++ app/FrbaCommerce/Servicios/Publicaciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text
+using FrbaCommerce.Modelos;
+
+namespace FrbaCommerce.Servicios
+{
+    class Publicaciones
+    {
+
+
+        public void guardar(Publicacion publicacion)
+        {
+        }
+
+
+    }
+}
