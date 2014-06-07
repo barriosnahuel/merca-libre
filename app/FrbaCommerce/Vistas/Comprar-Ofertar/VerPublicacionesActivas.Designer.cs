7225fceb86cd3c7d7c58f1904c284fdc24953c18
roman <romanglom@gmail.com>
2014-06-06 22:53:18 -0300

    Avancé con la parte de ver las publicaciones activas, no está terminado.

---
+++ app/FrbaCommerce/Vistas/Comprar-Ofertar/VerPublicacionesActivas.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace FrbaCommerce.Vistas.Comprar_Ofertar
+{
+    partial class VerPublicacionesActivas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listadoPublicaciones = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.admitePregunta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usuario_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.visibilidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.responder_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.listadoPublicaciones)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listadoPublicaciones);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(623, 386);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Publicaciones";
+            // 
+            // listadoPublicaciones
+            // 
+            this.listadoPublicaciones.AccessibleName = "listadoPublicaciones";
+            this.listadoPublicaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listadoPublicaciones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.admitePregunta,
+            this.usuario_id,
+            this.tipo,
+            this.estado,
+            this.visibilidad});
+            this.listadoPublicaciones.Location = new System.Drawing.Point(6, 19);
+            this.listadoPublicaciones.Name = "listadoPublicaciones";
+            this.listadoPublicaciones.Size = new System.Drawing.Size(610, 326);
+            this.listadoPublicaciones.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.Visible = false;
+            // 
+            // admitePregunta
+            // 
+            this.admitePregunta.DataPropertyName = "admitePregunta";
+            this.admitePregunta.HeaderText = "admitePregunta";
+            this.admitePregunta.Name = "admitePregunta";
+            this.admitePregunta.Visible = false;
+            // 
+            // usuario_id
+            // 
+            this.usuario_id.DataPropertyName = "usuario_id";
+            this.usuario_id.HeaderText = "usuario_id";
+            this.usuario_id.Name = "usuario_id";
+            this.usuario_id.Visible = false;
+            // 
+            // tipo
+            // 
+            this.tipo.DataPropertyName = "tipo";
+            this.tipo.HeaderText = "tipo_publicacion_id";
+            this.tipo.Name = "tipo";
+            this.tipo.Visible = false;
+            // 
+            // estado
+            // 
+            this.estado.DataPropertyName = "estado";
+            this.estado.HeaderText = "estado_id";
+            this.estado.Name = "estado";
+            this.estado.Visible = false;
+            // 
+            // visibilidad
+            // 
+            this.visibilidad.DataPropertyName = "visibilidad";
+            this.visibilidad.HeaderText = "visiblidad_id";
+            this.visibilidad.Name = "visibilidad";
+            this.visibilidad.Visible = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.responder_button);
+            this.groupBox1.Location = new System.Drawing.Point(12, 404);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(623, 55);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Acciones";
+            // 
+            // responder_button
+            // 
+            this.responder_button.AccessibleName = "preguntar_button";
+            this.responder_button.Location = new System.Drawing.Point(6, 19);
+            this.responder_button.Name = "responder_button";
+            this.responder_button.Size = new System.Drawing.Size(158, 23);
+            this.responder_button.TabIndex = 1;
+            this.responder_button.Text = "Hacer una pregunta";
+            this.responder_button.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "seleccionar_button";
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(525, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Seleccionar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // VerPublicacionesActivas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 470);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "VerPublicacionesActivas";
+            this.Text = "VerPublicacionesActivas";
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.listadoPublicaciones)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView listadoPublicaciones;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn admitePregunta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn usuario_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn estado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn visibilidad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button responder_button;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
