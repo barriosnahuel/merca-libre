3f74ec16bfc3ebcf36b62d7fc9eb4f26cf5295d1
kevin furman <kaf.com@hotmail.com>
2014-06-15 22:54:44 -0300

    el script de creacion estaba roto, lo arreglo

--- app/FrbaCommerce/Vistas/Facturar Publicaciones/frmPublicacionesAFacturar.cs
+++ app/FrbaCommerce/Vistas/Facturar Publicaciones/frmPublicacionesAFacturar.cs
@@ -24,46 +24,49 @@ namespace FrbaCommerce.Vistas.Facturar_Publicaciones
         {
             List<Publicacion> listaPublicaciones = Publicaciones.buscarSinFacturar(Session.usuario.id);
 
-            dgvPublicaciones.AutoGenerateColumns = false;
-
-            DataGridViewTextBoxColumn colNro = new DataGridViewTextBoxColumn();
-            colNro.DataPropertyName = "id";
-            colNro.HeaderText = "Nro publicacion";
-
-            DataGridViewTextBoxColumn colDescripcion = new DataGridViewTextBoxColumn();
-            colDescripcion.DataPropertyName = "descripcion";
-            colDescripcion.HeaderText = "Descripcion";
-
-            DataGridViewTextBoxColumn colUnidades = new DataGridViewTextBoxColumn();
-            colUnidades.DataPropertyName = "unidades";
-            colUnidades.HeaderText = "Unidades";
-
-            DataGridViewTextBoxColumn colPrecio = new DataGridViewTextBoxColumn();
-            colPrecio.DataPropertyName = "precio";
-            colPrecio.HeaderText = "Precio unidad";
-
-            DataGridViewTextBoxColumn colDesde = new DataGridViewTextBoxColumn();
-            colDesde.DataPropertyName = "desde";
-            colDesde.HeaderText = "Desde";
-
-            DataGridViewTextBoxColumn colHasta = new DataGridViewTextBoxColumn();
-            colHasta.DataPropertyName = "hasta";
-            colHasta.HeaderText = "Hasta";
-
-            dgvPublicaciones.Columns.Add(colNro);
-            dgvPublicaciones.Columns.Add(colDescripcion);
-            dgvPublicaciones.Columns.Add(colUnidades);
-            dgvPublicaciones.Columns.Add(colPrecio);
-            dgvPublicaciones.Columns.Add(colDesde);
-            dgvPublicaciones.Columns.Add(colHasta);
-
-            dgvPublicaciones.DataSource = listaPublicaciones;
-
-            for (int i = 0; i < listaPublicaciones.Count; i++)
+            if (listaPublicaciones.Count > 0)
             {
-                cmbCantPub.Items.Insert(i, i + 1);
+                dgvPublicaciones.AutoGenerateColumns = false;
+
+                DataGridViewTextBoxColumn colNro = new DataGridViewTextBoxColumn();
+                colNro.DataPropertyName = "id";
+                colNro.HeaderText = "Nro publicacion";
+
+                DataGridViewTextBoxColumn colDescripcion = new DataGridViewTextBoxColumn();
+                colDescripcion.DataPropertyName = "descripcion";
+                colDescripcion.HeaderText = "Descripcion";
+
+                DataGridViewTextBoxColumn colUnidades = new DataGridViewTextBoxColumn();
+                colUnidades.DataPropertyName = "unidades";
+                colUnidades.HeaderText = "Unidades";
+
+                DataGridViewTextBoxColumn colPrecio = new DataGridViewTextBoxColumn();
+                colPrecio.DataPropertyName = "precio";
+                colPrecio.HeaderText = "Precio unidad";
+
+                DataGridViewTextBoxColumn colDesde = new DataGridViewTextBoxColumn();
+                colDesde.DataPropertyName = "desde";
+                colDesde.HeaderText = "Desde";
+
+                DataGridViewTextBoxColumn colHasta = new DataGridViewTextBoxColumn();
+                colHasta.DataPropertyName = "hasta";
+                colHasta.HeaderText = "Hasta";
+
+                dgvPublicaciones.Columns.Add(colNro);
+                dgvPublicaciones.Columns.Add(colDescripcion);
+                dgvPublicaciones.Columns.Add(colUnidades);
+                dgvPublicaciones.Columns.Add(colPrecio);
+                dgvPublicaciones.Columns.Add(colDesde);
+                dgvPublicaciones.Columns.Add(colHasta);
+
+                dgvPublicaciones.DataSource = listaPublicaciones;
+
+                for (int i = 0; i < listaPublicaciones.Count; i++)
+                {
+                    cmbCantPub.Items.Insert(i, i + 1);
+                }
+                cmbCantPub.SelectedIndex = 0;
             }
-            cmbCantPub.SelectedIndex = 0;
         }
 
         private void btnVerFactura_Click(object sender, EventArgs e)
